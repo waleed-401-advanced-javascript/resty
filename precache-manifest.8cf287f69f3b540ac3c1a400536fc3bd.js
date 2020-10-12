736878ede69521fee1a4ee6f5cc0dfc13f843a43
waleedfarraj <waleed.farraj95@gmail.com>
2020-10-12 18:07:54 +0300

    Updates

---
+++ precache-manifest.8cf287f69f3b540ac3c1a400536fc3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e53bfb2f0ac5d1fd3bb08c8125efc92",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "3355cfa0ec889eea6054",
+    "url": "/resty/static/css/main.35a59966.chunk.css"
+  },
+  {
+    "revision": "6c7a6f1b6ab4c3a19d18",
+    "url": "/resty/static/js/2.12e94565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.12e94565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3355cfa0ec889eea6054",
+    "url": "/resty/static/js/main.0539d628.chunk.js"
+  },
+  {
+    "revision": "6972438e44fdcad64bce",
+    "url": "/resty/static/js/runtime-main.b658189f.js"
+  }
+]);
